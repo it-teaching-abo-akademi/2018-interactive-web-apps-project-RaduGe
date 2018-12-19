f206bc0269ad6ff2eece5113d5658f440646bbbb
Geacu Radu <geacuradu@gmail.com>
2018-12-20 00:52:01 +0200

    Updates

---
+++ precache-manifest.61792f7a2a7cbc80b69b50db8d74ee72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cb8fe21f8855f3fc3e1",
+    "url": "/2018-interactive-web-apps-project-RaduGe/static/js/runtime~main.8cb8fe21.js"
+  },
+  {
+    "revision": "5793d39cc62a45ff68ad",
+    "url": "/2018-interactive-web-apps-project-RaduGe/static/js/main.5793d39c.chunk.js"
+  },
+  {
+    "revision": "9586c0d5ce41fdd35511",
+    "url": "/2018-interactive-web-apps-project-RaduGe/static/js/1.9586c0d5.chunk.js"
+  },
+  {
+    "revision": "9586c0d5ce41fdd35511",
+    "url": "/2018-interactive-web-apps-project-RaduGe/static/css/1.3a87fc6a.chunk.css"
+  },
+  {
+    "revision": "919364c1b5254baeb0a0ba1171f4bb75",
+    "url": "/2018-interactive-web-apps-project-RaduGe/index.html"
+  }
+];
